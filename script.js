7d9fe510125a53f22302d1054e8759629ea4c1c9
Thamitha <thamitharajput@gmail.com>
2022-12-21 06:56:07 +0530

    Handling Errors

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
  function fetchRandomDogImage(){
-// method:1
+// //method:1(plain venila js call)
 //     var xhrRequest = new XMLHttpRequest();
 
 //     xhrRequest.onload = function(){
-//         console.log(xhrRequest.response);
+//          console.log(xhrRequest.response);
 //         var responseJSON = JSON.parse(xhrRequest.response);
 //         var imageUrl = responseJSON.message;
 //         $('#dog-image').attr('src',imageUrl);
 //     };
+//     xhrRequest.onerror = function(){
+//         console.log("Request Failed");
+//     };
 //     xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
-//     xhrRequest.send();
+//     xhrRequest.send('');
 
 // }
 
@@ -19,22 +22,28 @@
 //     url:'https://dog.ceo/api/breeds/image/random',
 //     method:'GET',
 //     //data:is response.data already in JSON format
-//     success:function(data){{
+//     success:function(data){
 //         // var responseJSON = JSON.parse(xhrRequest.response);
 //         var imageUrl = data.message;
 //         $('#dog-image').attr('src',imageUrl); 
-//     }}
+//     },
 
+// }).fail(function(){
+//     console.log('error');
 // });
-
+ 
 
 
 // method:3
-
+// xhr here is jQuery xhr
 $.get('https://dog.ceo/api/breeds/image/random',function(data){
             var imageUrl = data.message;
             $('#dog-image').attr('src',imageUrl); 
+ }).fail(function(xhr, textStatus, errorThrown){
+    console.log("Request Failed");
  });
-}
+ }
+
+
 // handel button-click
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
